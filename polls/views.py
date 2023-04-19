ab97930a998c4b2b979cde8adc27668d16b2e444
Lee-Dong-Min <dongmin733@gmail.com>
2023-04-19 16:00:26 +0900

    question form prototype

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+def index(request):
+    context = {'title': 'Home'}
+    return render(request, 'polls/index.html', context)
+
+def detail(request):
+    context = {'title': 'choice'}
+    return render(request, 'polls/detail.html', context)
+
+@require_POST
+def results(request):
+    result = list(request.POST.values())[1:]
+    context = {'title': 'result and info', 'result': result}
+    return render(request, 'polls/results.html', context)
