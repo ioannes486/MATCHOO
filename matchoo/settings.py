9978ae826078707f5b0fe4e922236635ecdc05a9
Lee-Dong-Min <dongmin733@gmail.com>
2023-04-19 10:46:34 +0900

    settings.py minor patch

--- matchoo/settings.py
+++ matchoo/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls,'
+    'polls',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
