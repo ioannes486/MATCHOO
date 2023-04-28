39729cf54808cf122043085c1d4a5f831b303a23
DESKTOP-081J6RT\TECH2_24 <rhkdghks21@naver.com>
2023-04-28 13:35:02 +0900

    chatbot complete

--- polls/views.py
+++ polls/views.py
@@ -1,21 +1,51 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.http import Http404, HttpResponse, HttpResponseRedirect, JsonResponse
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
-
+import json
+import openai
 
 def index(request):
     context = {"title": 'Home'}
-    return render(request, "polls/index.html", context)
+    print(request)
 
+    return render(request, "polls/index.html", context)
+    
+
+    
 def detail(request):
     return render(request, "polls/detail.html", {"title": 'question'})
 
+# 이거 꼭 바꿔라!!!!!!!!!!!!! 안바꾸면 3대멸망 아래로 3대임 내 대는 아님
+openai.api_key = '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'
+
 def results(request):
-    result = list(request.POST.values())[1:]
-    context = {'title': 'result and info', 'result': result}
-    return render(request, "polls/results.html", context)
+    if request.method == 'POST':
+        # 요청 받아서 문구 만들기
+        res = request.POST
+        user_message = '나는 {}고 {}고 {}고 {}고 {}고 {}해 나한테 부연설명 없이 단답형으로 한국여행지 한 곳을 추천해줘'.format(res.get('trip'), res.get('type'), res.get('movement'), res.get('pop'), res.get('with'), res.get('how'), )
+
+        # 만든 문구로 챗봇에게 넘겨주기
+        messages = [{'role': 'user', 'text': user_message}]
+        completion = openai.Completion.create(
+            model='text-davinci-003',
+            prompt='\n'.join([f'{m["role"]}: {m["text"]}' for m in messages]),
+            temperature=0, # 얼마나 자연스럽게 답을 줄건지에 대해 높을수록 자연스러운 대화 가능
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            timeout=15,
+        )
+        assistant_message = completion.choices[0].text.strip()
+        messages.append({'role': 'assistant', 'text': assistant_message})
+        bot_message = messages[1]['text'].strip('!')
+        print(res)
+        dir(res)
+        #return messages
+    
+        return render(request, 'polls/results.html', {'bot_message': bot_message})
+        #return render(request, "polls/results.html", context)
 
 def vote(request):
     pass
