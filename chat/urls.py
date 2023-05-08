d7f121d6673ec8b7d34ca6294ce72eaacfcd7b1b
DESKTOP-081J6RT\TECH2_24 <rhkdghks21@naver.com>
2023-05-08 09:55:08 +0900

    deleted chat app & add protobuf in requirements

--- chat/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'chat'
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('generate_text/', views.generate_text, name='generate_text'),
-]
--- matchoo/settings.py
+++ matchoo/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django_extensions',
     'polls',
-    #'chat',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
